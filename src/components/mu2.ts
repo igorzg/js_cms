d79443fd705a74cc907cc2b2a789d65fbb6f5058
igorzg <igor.zg1987@gmail.com>
2018-08-13 13:45:58 +0200

    Update of template path resolver

--- src/components/mu2.ts
+++ src/components/mu2.ts
@@ -2,9 +2,10 @@ import {normalize} from "path";
 import {Injectable, isUndefined} from "@typeix/rexxar";
 import {compileAndRender} from "mu2";
 
-if (isUndefined(process.env.VIEWS_PATH)) {
-  process.env.VIEWS_PATH = "/build/views/";
+if (isUndefined(process.env.BUILD_PATH)) {
+  process.env.BUILD_PATH = "/build/";
 }
+
 /**
  * Template engine
  * @constructor
@@ -16,27 +17,28 @@ if (isUndefined(process.env.VIEWS_PATH)) {
  */
 @Injectable()
 export class TemplateEngine {
-    /**
-     * Gets template path
-     * @return {String}
-     */
-    static getTemplatePath(name: String): string {
-        return normalize(process.cwd() + process.env.VIEWS_PATH + name + ".mustache");
-    }
+  /**
+   * Gets template path
+   * @return {String}
+   */
+  static getTemplatePath(name: String, path?: string): string {
+    return normalize(process.cwd() + process.env.BUILD_PATH + (isUndefined(path) ? "views/" : path) + name + ".mustache");
+  }
 
-    /**
-     * Load template from disk
-     * @param template
-     * @param data
-     * @returns {NodeJS.ReadableStream}
-     */
-    compileAndRender(template: String, data: any): Promise<string> {
-        return new Promise((resolve, reject) => {
-            let buffer = "";
-            compileAndRender(TemplateEngine.getTemplatePath(template), data)
-                .on("data", (chunk) => buffer += chunk.toString())
-                .on("error", error => reject(error))
-                .on("end", () => resolve(buffer));
-        });
-    }
+  /**
+   * Load template from disk
+   * @param template
+   * @param data
+   * @param path
+   * @returns {NodeJS.ReadableStream}
+   */
+  compileAndRender(template: String, data: any, path?: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let buffer = "";
+      compileAndRender(TemplateEngine.getTemplatePath(template, path), data)
+        .on("data", (chunk) => buffer += chunk.toString())
+        .on("error", error => reject(error))
+        .on("end", () => resolve(buffer));
+    });
+  }
 }
